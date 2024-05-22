985a032c7769cd8f845351c17e39b92b0772d3e5
unknown <anikool.saini@gmail.com>
2024-05-22 10:42:41 +0530

    Final commit

---
+++ game/Assets/Scripts/Player movement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    [Header("Slope Movement")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    public float playerHeight;
+
+    public Transform orientation;
+    float horizontalInput;
+    float verticalInput;
+    float petInput;
+    public Animator anim;
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+    private Vector3 GetSlopeMoveDirection(){
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
+    }
+    private void SpeedControl(){
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if(flatVel.magnitude > moveSpeed){
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Update(){
+        MyInput();
+        SpeedControl();
+    }
+
+    private void FixedUpdate(){
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer(){
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        anim.SetFloat("isMoving", moveDirection.magnitude);
+    }
+
+}
